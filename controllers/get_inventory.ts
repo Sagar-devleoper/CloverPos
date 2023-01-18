723ae2a33aa6fba07741e4f4c2e06a38b5bd7087
Sagar Singh <sagardeveloper08@gmail.com>
2023-01-18 14:08:17 +0530

    update the code

--- controllers/get_inventory.ts
+++ controllers/get_inventory.ts
@@ -21,32 +21,42 @@ const getInventroy = async (req: Request, res: Response, next: NextFunction) => 
         const url = " https://sandbox.dev.clover.com/v3/merchants/HBVBZZTDA2341/items?expand=tags&expand=categories&expand=taxRates&expand=modifierGroups&expand=itemStock&expand=options&expand=menuItem&orderBy=name%20ASC&limit=26"
         let result = await axios.get(url, config)
         if (result) {
-            console.log(result.data.elements)
+            // console.log(result.data.elements)
             let data = result.data.elements
-            for (const datas of data) {
+            console.log(data, '26')
+            data.forEach(async (element: any) => {
+                console.log(element, 'element')
+                const findInvetory = await Inventory.findOne({
+                    where: {
+                        inventoryId: element.id
+                    }
+                })
+                if (findInvetory) {
+                    return res.status(201).json({
+                        message:"data already exist"
+                    })
+                }
                 const vale = await Inventory.create({
                     id,
-                    inventoryId: datas.id,
-                    name: datas.name,
-                    price: datas.price
+                    inventoryId: element.id,
+                    name: element.name,
+                    price: element.price
                 })
-
-                if (vale) {
-                    res.status(200).json({
-                        message: "record inserted succefully",
-                        
+                console.log(vale, 'vale')
+                if (vale !== null) {
+                    return res.status(200).json({
+                        message: "record inserted succesfully",
+                        // data:vale
                     })
-                    return
                 }
-            }
-
+            });
             return
         }
     } catch (error) {
+        console.log(error)
         res.status(500).json({
             message: "something went wrong"
         })
-        console.log(error)
     }
 
 }
